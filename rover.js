da0bee8150dd44ae2a35390262f92f40bd0879f6
ausates <81811078+ausates@users.noreply.github.com>
2021-07-01 18:49:27 +0000

    tests 9 and 10 complete. working on 11

--- rover.js
+++ rover.js
@@ -3,20 +3,34 @@ class Rover {
 
    constructor(position){
      this.position = position;
-
+     this.mode = 'NORMAL';
+     this.generatorWatts = 110;
      if (!this.position){
        throw Error("Position required.");
      }
-     this.mode = 'NORMAL';
-     this.generatorWatts = 110;
    }
+  //  roverStatus = [this.position, this.mode, this.generatorWatts];
    receiveMessage(message){
     let messageObj = {
       name: message.name,
-      results: [message.commands]
-    };
+      results: message.commands,
+      };
+      for (let i=0; i < messageObj.results.length; i++){
+      if (messageObj.results[i]['commandType'] === 'STATUS_CHECK'){
+        this.roverStatus = [this.mode, this.generatorWatts, this.position];
+      } 
+      if (messageObj.results[i]['commandType'] === 'MODE_Change'){
+       if(messageObj.results[i]['value'] === 'LOW_POWER'){
+         this.mode = 'NORMAL';
+         this.completed = true;
+    } else if(messageObj.results[i]['value'] === 'NORMAL'){
+        this.mode = 'LOW_POWER';
+        messageObj.completed = true;
+        }
+      }
+    } 
+    
     return messageObj;
    }
 }
-
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -6,18 +6,32 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Rover class", function(){
+  let commands = [new Command('STATUS_CHECK')];
+  let message = new Message('Test message with two commands', commands);
+  let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+  let response = rover.receiveMessage(message);
 
   it('constructor sets position and default values for mode and generatorWatts', function(){
-    let rover = new Rover(99999);
     let defaults = [rover.position, rover.mode, rover.generatorWatts];
-    expect(defaults).toEqual([99999, 'NORMAL', 110])
-  })
+    expect(defaults).toEqual([98382, 'NORMAL', 110])
+  });
 
   it('response returned by receiveMessage contains name of message', function(){
-    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-    let message = new Message('Test message with two commands', commands);
-    let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-    let response = rover.receiveMessage(message);
+    
     expect(response.name).toEqual('Test message with two commands');
+  });
+
+  it('response returned by receiveMessage includes two results if two commands are sent in the message', function(){
+    expect(response.results.length).toEqual(2);
+  });
+
+  it('responds correctly to status check command', function(){
+    expect(rover.roverStatus).toEqual([rover.mode, rover.generatorWatts, rover.position]);
   })
+
+  it('responds correctly to mode change command', function(){
+   /*This works because default mode is normal, and the command above is 'MODE_CHANGE'*/ 
+   expect(response.completed).toEqual(true);
+  })
+  
 });
